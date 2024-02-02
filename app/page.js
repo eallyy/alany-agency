65acffbaa4fb0634fbd3605108bb089e456953b5
eallyy <eallyy@icloud.com>
2024-02-02 17:06:23 +0300

    Calendly responsive design added.

--- app/page.js
+++ app/page.js
@@ -15,36 +15,36 @@ export default function Home() {
         />
       </header>
       <main className="flex-grow overflow-y-auto overflow-x-hidden">
-        <section className="container mx-auto w-full md:py-12 flex justify-center items-center">
-          <div>
-            <iframe
-              src="https://player.vimeo.com/video/896231378?h=66672d7d53"
-              width="640"
-              height="360"
-              frameBorder="0"
-              allow="autoplay; fullscreen; picture-in-picture"
-              className="w-screen sm:h-96"
-              allowFullScreen>
-            </iframe>
+        <section className="container mx-auto w-full px-2 justify-center items-center text-center
+        md:py-6 md:pb-12
+        pb-6
+        ">
+          <div className="col-span-12">
+            <p className="font-bold uppercase text
+            text-3xl
+
+            ">More Than Products, It's People</p>
+
+          </div>
+          <div className="col-span-12">
+            <p className="font-normal uppercase
+            text-xl
+            sm:text-3xl
+            ">Cultivate Lasting Impact with Alany's Holistic Approach</p>
+
           </div>
         </section>
 
 
-        <section className="container mx-auto w-full py-12 grid lg:grid-cols-2 items-center">
-          <div className="col-span-1 px-6">
-            <Image
-              src="/what_we_do.jpg"
-              width={512}
-              height={512}
-              sizes="100vw"
-              style={{ width: '100%', height: 'auto' }}
-              alt="Gabrielle Chanel Paris Photoshoot"
-            />
-          </div>
-          <div className="col-span-1 py-6 px-6 text-white">
-            <p className="text-5xl font-black uppercase mb-4">What We Do for your business?</p>
-            <p className="text-2xl font-normal mb-4">We take new clients to your door through paid advertisement on Google, Facebook, Instagram and YouTube.
-              This way, you can focus on what really matters in your business - without having to worry about where your next client is coming from.</p>
+        <section className="container mx-auto w-full md:px-60 flex justify-center items-center">
+          <div className="aspect-video w-screen">
+            <iframe
+              src="https://player.vimeo.com/video/896231378?h=66672d7d53"
+              frameBorder="0"
+              allow="autoplay; fullscreen; picture-in-picture"
+              className="w-full h-full"
+              allowFullScreen>
+            </iframe>
           </div>
         </section>
 
@@ -67,9 +67,33 @@ export default function Home() {
           </div>
         </section>
 
+
+        <section className="container mx-auto w-full grid lg:grid-cols-2 items-center">
+          <div className="col-span-1 px-6">
+            <Image
+              src="/what_we_do.jpg"
+              width={512}
+              height={512}
+              sizes="100vw"
+              style={{ width: '100%', height: 'auto' }}
+              alt="Gabrielle Chanel Paris Photoshoot"
+            />
+          </div>
+          <div className="col-span-1 py-6 px-6 text-white">
+            <p className="text-5xl font-black uppercase mb-4">What We Do for your business?</p>
+            <p className="text-2xl font-normal mb-4">We take new clients to your door through paid advertisement on Google, Facebook, Instagram and YouTube.
+              This way, you can focus on what really matters in your business - without having to worry about where your next client is coming from.</p>
+          </div>
+        </section>
+
+
         <section className="container mx-auto w-full py-12 flex justify-center items-center">
           <div
-            className="calendly-inline-widget h-svh w-full"
+            className="calendly-inline-widget w-full
+            h-[800px]
+            md:h-[900px]
+            lg:h-[700px]
+            "
             data-url="https://calendly.com/promiroffical/30min?background_color=000000&text_color=ffffff&primary_color=00d2ff">
           </div>
           <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
